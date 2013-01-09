2b001c91f2a72ef3f9461fb7ee1fa33f4b3e7b62
Baptiste Fontaine <batifon@yahoo.fr>
2013-01-09 15:31:12 +0100

    Refactoring

--- src/jquery.ajaxedit.js
+++ src/jquery.ajaxedit.js
@@ -11,6 +11,7 @@
  **/
 ;(function( $ ) {
 
+
     var defaultOptions = {
          
             /* Define when the element will be set to editable mode.
@@ -37,24 +38,14 @@
             },
 
             // buttons' labels
-            buttons: {
+            labels: {
                 edit:   'Edit',
                 save:   'Save',
                 cancel: 'Cancel'
-            },
-
-            // if this is set to true, the fields' text values will
-            // be preloaded. If this is set to false, their text values
-            // will be loaded only if the user try to edit them.
-            prefetch: false // #NotImplemented
+            }
 
         },
 
-
-        // This Error is raised when no URL is provided for
-        // getting/posting the content
-        noURLException = new Error( 'No URL provided.' ),
-                        
         // random key used for unique class attributes
         key = '__' + ( 0|Math.random() * 1000 ) + 'k' + +new Date(),
 
@@ -75,242 +66,49 @@
                         .addClass( editor_class );
 
 
-    // attach an "Edit" button to the element, which will be made
-    // visible when the user move their mouse over the element
-    function attachHoverButton( $el, label, callback ) {
-
-        var $editButton = $baseButton.clone()
-                            .attr( 'value', label )
-                            .click( callback );
-
-        $el.hover(
-
-            function() {
-            
-                if ( !$el.data( 'edited' ) ) {
-                
-                    addButton( $el, $editButton );
-                
-                }
-
-            },
-
-            function() { removeButton( $el, $editButton ); }
-        );
-
-    }
-
-    // Add a button on an element
-    function addButton( $el, $button ) {
-        
-        $button.appendTo( $el );
-
-    }
-
-    // Remove a button which is on an element
-    function removeButton( $el, $button ) {
-        
-        $button.detach();
-
-    }
-
-    // called when the user click the "Save" button on an element
-    // (return a callback)
-    function saveEdit( opts, $el ) {
-
-        return function( ev ) {
-        
-            //TODO
-        
-        };
-
-    }
-
-    // called when the user cancel an edit on an element
-    // (return a callback)
-    function cancelEdit( opts, $el ) {
-
-        return function( ev ) {
-        
-            $el.find( button_class ).remove().end()
-               .find( editor_class ).remove().end()
-               .data( 'edited', false )
-               .trigger( 'EditCanceled' )
-               .html( $el.data( 'html' ) || '' );
-
-        };
-
-    }
-
-    // called when the user try to edit an element
-    // (return a callback)
-    function edited( opts, $el ) {
-    
-        return function() {
-
-            if ( $el.data( 'edited' ) ) { return; }
-
-            $el.trigger( 'EditInit' )
-               .data( 'edited', true );
-
-            fetch( opts )( $el,
-
-                 // called with the data from the server
-                 function( data ) {
-
-                     var text;
-
-                     if ( !$el.data( 'edited' ) ) { return; }
-
-                     text = data[ $el.data( 'name' ) ][ opts.fields.text ];
-
-                     $el.trigger( 'EditOk' )
-                        .text( '' )
-                        .data( 'html',
-                               data[$el.data( 'name' )][opts.fields.html] );
-
-                     // "Cancel" button
-                     addButton( $el, $baseButton.clone()
-                                       .attr( 'value', opts.buttons.cancel )
-                                       .click( cancelEdit( opts, $el ) ) );
-
-                     // "Save" button
-                     addButton( $el, $baseButton.clone()
-                                       .attr( 'value', opts.buttons.save )
-                                       .click( saveEdit( opts, $el ) ) );
-
-                     $el.append( $baseEditor.clone().text( text ) );
-                 
-                 },
-                 // called if there's an error
-                 function( err ) {
-
-                    if ( !$el.data( 'edited' ) ) { return; }
-                 
-                    $el.data( 'edited', false )
-                       .trigger('EditError');
-
-                    //TODO callback?
-                 
-                 }
-            );
-        };
-    }
-
-    // fetch $el's data from the server and pass it to
-    // the callback function
-    // (return a callback)
-    function fetch( opts ) {
-
-        return function( $el, successCallback, errorCallback ) {
-
-            // optional additional parameters
-            var params = $el.data( 'params' ) || {};
-
-            $.ajax( opts.url, {
-
-                data: params,
-                context: $el,
-                success: successCallback,
-                error: errorCallback
-            
-            });
-
-        };
-    
-    }
-
     // AjaxEdit main function
-    $.fn.ajaxedit = function( opts ) {
+    $.fn.ajaxedit = function( o ) {
 
-        if ( arguments.length === 0 ) {
-
-            throw noURLException;
-
-        }
+        var opts = {},
+            url;
 
         // if the argument is a string, we assume that
         // this is the API endpoint (URL)
-        if ( typeof opts === 'string' ) {
+        if ( typeof o === 'string' ) {
 
-            opts = $.extend( true, {}, defaultOptions, { url: opts } );
+            opts = $.extend( true, {}, defaultOptions, { url: o } );
 
         }
         // else, this is an object where keys/values are options
-        else if ( typeof opts === 'object' ) {
+        else if ( typeof o === 'object' ) {
 
-            opts = $.extend( true, {}, defaultOptions, opts );
-
-        }
-
-        if ( !opts.hasOwnProperty('url') || !opts['url'] ) {
-
-            throw noURLException;
+            opts = $.extend( true, {}, defaultOptions, o );
 
         }
 
-        var fields = [];
+        url = opts.url || opts.uri;
 
-        if ( this.length === 1 && this.attr('data-name') ) {
-        
-            fields.push( this );
-        
-        }
-        else {
+        this.each(function( i, e ) {
 
-            this.each( function( i, e ) {
+            var $e = $( e ), u;
 
-                var $e = $(e);
+            if (u = ( $e.data( 'fetchUrl' ) || $e.data( 'fetchUri' ) ) ) {
 
-                if ( $e.attr( 'data-name' ) ) {
+                $e.data( 'ajaxedit.url', u.trim() );
 
-                    // if this element is editable, don't search for fields in it
-                    fields.push($e);
+            } else if ( url ) {
 
-                }
-                else {
+                $e.data( 'ajaxedit.url', url.trim() );
 
-                    // otherwise, pick its children
-                    fields = fields.concat( $e.find( '[data-name]' ) );
+            } else {
 
-                }
+                throw new Error( 'No URL provided.' );
 
-            });
+            }
 
-        }
-
-        // if there is no editable elements, don't do anything
-        if ( fields.length === 0 ) { return this; }
-
-        // List of events triggered when the user wants to edit an element
-        var editOn = ( $.type( opts.editOn ) === 'array' ) ?
-                            opts.editOn :
-                                [ opts.editOn ];
-
-        $.each( fields, function( i, $field ) {
-
-            // For each 'edit' event, bind the event to the callback
-            $.each( editOn, function( i, ev ) {
-
-                // special 'event': The "Edit" button
-                if ( ev === 'hoverButton' ) {
-
-                    attachHoverButton( $field, opts.buttons.edit, edited(opts, $field) );
-                
-                }
-                else {
-
-                    $field.bind( ev, edited(opts, $field) );
-
-                }
-
-            });
-
-            // The field is ready to be edited
-            $field
-                .attr( 'contenteditable', false )
-                .trigger( 'EditReady' );
         });
+
+        //TODO
 
         return this;
 
--- test/tests.js
+++ test/tests.js
@@ -1,168 +1,48 @@
 (function($) {
 
-    // empty text
-    $.mockjax({
-        url: '/api/no-content.json',
-        responseText: {
-            the_text: {
-                html: '',
-                text: ''
-            }
-        }
-    });
+    // globals
+    var g = {};
 
-    // one field
-    $.mockjax({
-        url: '/api/hello.json',
-        responseText: {
-            the_text: {
-                html: '<p>Hello</p>',
-                text: 'Hello'
-            }
-        }
-    });
+    describe( 'Initialization', function() {
 
-    var $baseDiv = $( '<div>' )
-                        .attr( 'data-name', 'the_text' )
-                        .html( '<p>Hello</p>' ),
-        $theDiv,
-        body     = document.body;
+        beforeEach(function() {
 
-    test( 'no element', function() {
+            g.$baseDiv = $( '<div></div>' )
+                                .html( '<p>Hello!</p>' );
 
-        ok( $( '#no-elem' ).ajaxedit( 'foo' ) );
+        });
 
-    });
+        it( 'should accept the URL as an `uri` parameter', function() {
 
-    test( 'no URL', function() {
+            expect(function() { g.$baseDiv.ajaxedit({ uri: 'foo' }); }).to.not.throw(Error);
 
-        $theDiv = $baseDiv.clone().appendTo(body);
+        });
 
-        throws( function() { $theDiv.ajaxedit();   }         , Error);
-        throws( function() { $theDiv.ajaxedit(''); }         , Error);
-        throws( function() { $theDiv.ajaxedit({}); }         , Error);
-        throws( function() { $theDiv.ajaxedit({ url: '' }); }, Error);
+        it( 'should accept the URL as an `uri` parameter', function() {
 
-        $theDiv.remove();
-    });
+            expect(function() { g.$baseDiv.ajaxedit({ url: 'foo' }); }).to.not.throw(Error);
 
-    test( 'hover button', function() {
+        });
 
-        $theDiv = $baseDiv.clone().appendTo(body);
+        it( 'should accept the URL as a `data-fetch-url` attribute', function() {
 
-        ok(
-            $theDiv.ajaxedit({
-                url: '/api/no-content.json'
-            })
-        );
+            g.$baseDiv.attr( 'data-fetch-url', 'foo' );
+            expect(function() { g.$baseDiv.ajaxedit(); }).to.not.throw(Error);
 
-        ok( $( 'input[type="button"]', $theDiv ).length === 0 );
+        });
 
-        $theDiv.trigger( 'mouseenter' );
-        ok( $( 'input[type="button"]', $theDiv ).length === 1 );
+        it( 'should accept the URL as a `data-fetch-uri` attribute', function() {
 
-        $theDiv.trigger( 'mouseleave' );
-        ok( $( 'input[type="button"]', $theDiv ).length === 0 );
+            g.$baseDiv.attr( 'data-fetch-uri', 'foo' );
+            expect(function() { g.$baseDiv.ajaxedit(); }).to.not.throw(Error);
 
+        });
 
-        $theDiv.remove();
+        it( 'should return itself', function() {
 
-    });
-
-    test( 'edit on 1 event', function() {
-
-        $theDiv = $baseDiv.clone().appendTo(body);
-
-        ok(
-            $theDiv.ajaxedit({
-                url: '/api/no-content.json',
-                editOn: 'mouseover'
-            })
-        );
-
-        $theDiv.trigger( 'mouseover' );
-
-        $theDiv.remove();
-
-    });
-
-    test( 'edit on multiple events', function() {
-
-        $theDiv = $baseDiv.clone().appendTo(body);
-
-        ok(
-            $theDiv.ajaxedit({
-                url: '/api/no-content.json',
-                editOn: ['mouseover', 'hoverButton', 'click']
-            })
-        );
-
-        $theDiv.trigger( 'mouseover' );
-
-        $theDiv.remove();
-
-    });
-
-    test( 'bad url', function() {
-
-        $theDiv = $baseDiv.clone().appendTo(body);
-
-        ok(
-            $theDiv.ajaxedit({
-                url: '/api/bad-url'
-            })
-        );
-
-        // there should be no exception
-        $theDiv.trigger( 'mouseenter' );
-
-        $theDiv.remove();
-
-    });
-
-    test( 'edit via hover button click', function() {
-
-        $theDiv = $baseDiv.clone().appendTo(body);
-
-        ok(
-            $theDiv.ajaxedit({
-                url: '/api/hello.json'
-            })
-        );
-
-        ok( $( 'input[type="button"]', $theDiv).length === 0 );
-
-        $theDiv.trigger( 'mouseenter' );
-        ok( $( 'input[type="button"]', $theDiv).length === 1 );
-
-        // click on "Edit": two buttons should be displayed: "Cancel" & "Save"
-        $( 'input[type="button"]', $theDiv ).first().trigger( 'click' );
-
-        // waiting for the fake server response
-        stop();
-        setTimeout(function() {
-
-            ok( $( 'input[type="button"]', $theDiv ).length === 2 );
-            ok( $theDiv.find( 'div[contenteditable]' ).length === 1 );
-            
-            start();
-
-            // $theDiv.remove();
-
-        }, 500);
-
-
-    });
-
-    test( 'Cancel edit', function() {
-
-        // $theDiv = $baseDiv.clone().appendTo(body);
-
-        $( 'input[type="button"]', $theDiv ).first().trigger( 'click' );
-
-        ok( $( 'input[type="button"]', $theDiv ).length === 1 );
-        ok( $theDiv.attr( 'contenteditable' ) === 'false' );
-        ok( $theDiv.html() === '<p>Hello</p>' );
+            expect( g.$baseDiv.ajaxedit({ url: 'foo' }) ).to.deep.equal( g.$baseDiv );
+        
+        });
 
     });
 
